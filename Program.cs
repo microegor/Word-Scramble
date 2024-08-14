cf02aa6a2709da731b9db536a0a961cef0a27b4e
microegor <microegor321@gmail.com>
2024-08-14 14:51:37 +0200

    chore: Add Word Scramble game code

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "hello", "wonderful", "linq", "beautiful", "world" };
+
+
+        // Console.WriteLine($"Random word: {randomWord}");
+        Console.WriteLine("Добро пожаловать в игру!");
+
+        while (true)
+        {
+            string randomWord = RandomWord(words);
+            string shuffledWord = Shuffle(randomWord);
+            Console.Write("Вам нужно угадать слово из букв: ");
+            Console.WriteLine(shuffledWord);
+            while (true)
+            {
+                Console.Write("Введите ваш ответ: ");
+                string answer = Console.ReadLine();
+                if (answer == randomWord)
+                {
+                    Console.WriteLine("Поздравляю! Вы угадали слово!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("К сожалению, вы не угадали слово. Попробуйте еще раз.");
+                }
+            }
+            Console.WriteLine("Хотите сыграть еще раз? (да/нет)");
+            string playAgain = Console.ReadLine();
+            if (playAgain != "да" && playAgain != "yes" && playAgain != "y")
+            {
+                break;
+            }
+        }
+    }
+
+    static string RandomWord(string[] words)
+    {
+        Random random = new Random();
+        int randomIndex = random.Next(0, words.Length);
+        return words[randomIndex];
+    }
+
+    static string Shuffle(string word)
+    {
+        Random random = new Random();
+        char[] characters = word.ToCharArray();
+        for (int i = 0; i < characters.Length; i++)
+        {
+            int j = random.Next(0, characters.Length);
+            char temp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = temp;
+        }
+        return new string(characters);
+    }
+}
